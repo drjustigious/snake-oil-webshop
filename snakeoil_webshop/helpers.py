f52913c9264bcc7af2268b3870da921e79b34973
Jussi Tikkanen <drjustigious@gmail.com>
2021-05-06 20:48:30 +0300

    Give demo staff user the correct permissions.
    - Also create shopping cart when needed if it does not exist yet.

--- snakeoil_webshop/helpers.py
+++ snakeoil_webshop/helpers.py
@@ -3,10 +3,9 @@ from snakeoil_webshop.models import ShoppingCart
 def find_active_cart_for_user(user):
     """
     Returns the active ShoppingCart of the given User.
-    Right now every user has at most just one cart.
+    Right now every user has at most just one cart,
+    so we will also create a cart here if it does not
+    exist yet.
     """
-    try:
-        active_cart = ShoppingCart.objects.get(user=user)
-        return active_cart
-    except ShoppingCart.DoesNotExist:
-        return None
+    active_cart, created = ShoppingCart.objects.get_or_create(user=user)
+    return active_cart
--- snakeoil_webshop/management/commands/add_demo_users.py
+++ snakeoil_webshop/management/commands/add_demo_users.py
@@ -16,8 +16,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # Create the staff user. They have full access to webshop functions and the Django Admin.
         staff_user = self.update_user("demo.webmaster")
-        staff_user.is_staff = True
-        staff_user.save()
+        self.give_staff_permissions(staff_user)
 
         # Create the shop manager. They have full webshop feature access, but can't see Django Admin.
         # This part also configures the shop manager permission group.
@@ -49,3 +48,29 @@ class Command(BaseCommand):
         user.set_password(password)
 
         return user
+
+
+    @staticmethod
+    def give_staff_permissions(user):
+        """
+        Promotes the given user to a staff member with full
+        privileges over the web shop's resources.
+        """
+        # Create a staff user group if it didn't exist yet.
+        staff_group, created = Group.objects.get_or_create(name="shop.staff")
+
+        # Give the staff group all permissions over the webshop resources.
+        nouns = ["product", "shoppingcart", "shoppingcartitem"]
+        verbs = ["add", "change", "delete", "view"]
+
+        for noun in nouns:
+            for verb in verbs:
+                permission_codename = f"{verb}_{noun}"
+                staff_group.permissions.add(
+                    Permission.objects.get(codename=permission_codename)
+                )
+
+        # Add the given user to the staff group.
+        staff_group.user_set.add(user)                
+        user.is_staff = True
+        user.save()
