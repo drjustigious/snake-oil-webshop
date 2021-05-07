bba68486e458747ccc2a9a470c08d850616314e3
Jussi Tikkanen <44530293+drjustigious@users.noreply.github.com>
2021-05-07 20:44:45 +0300

    Include defaults for critical environment variables

--- snakeoil/wsgi.py
+++ snakeoil/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import sys
+import os
 from django.core.wsgi import get_wsgi_application
 
 sys.path.append('/opt/bitnami/projects/snake-oil-webshop')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/projects/snake-oil-webshop/egg_cache")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "snakeoil.settings")
 
 application = get_wsgi_application()
